cf183538bbb9ba82690a13ca890697534bb60bce
aalnaj2s <aya.alnajjar@smail.inf.h-brs.de>
2022-10-19 00:21:31 +0200

    Erste Erstellung einer Factory noch ohne Methode

---
+++ src/org/hbrs/s1/ws22/uebung1/control/TranslatorFactory.java
@@ -0,0 +1,8 @@
+package org.hbrs.s1.ws22.uebung1.control;
+
+public class TranslatorFactory {
+    public static GermanTranslator createGermanTranslator(){
+
+        return new GermanTranslator();
+    }
+}
